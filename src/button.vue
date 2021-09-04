61c34cb69a66078527b60a7b7b84e1c8ba7d24fe
薛斌 <928691436@qq.com>
2021-09-04 20:36:37 +0800

    添加icon和position属性

--- src/button.vue
+++ src/button.vue
@@ -1,9 +1,18 @@
 <template>
-  <button class="g-button">按钮</button>
+  <button class="g-button" :class="{[`icon-${position}`]: true}">
+    <svg v-if="icon" class="icon" aria-hidden="true">
+      <use :xlink:href="`#icon-${icon}`"></use>
+    </svg>
+    <div class="content">
+      <slot />
+    </div>
+  </button>
 </template>
 
 <script>
-export default {};
+export default {
+  props: ["icon", "position"],
+};
 </script>
 
 <style lang="scss">
@@ -14,6 +23,10 @@ export default {};
   border-radius: var(--border-radius);
   border: 1px solid var(--border-color);
   background: var(--button-bg);
+  display: inline-flex; //转为弹性盒模型
+  justify-content: center;
+  align-items: center;
+  vertical-align: middle;
   &:hover {
     border-color: var(--border-color-hover);
   }
@@ -22,6 +35,23 @@ export default {};
   }
   &:focus {
     outline: none;
+  }
+  > .content{
+      order: 2;
+  }
+  >.icon{
+      order: 1;
+      margin-right: .1em;
+  }
+  &.icon-right{
+      >.content{
+          order: 1;
+      }
+      >.icon{
+          order: 2;
+          margin-right: .1em;
+          margin-left: 0
+      }
   }
 }
 </style>
