76b9b68a773a293186dfe2c8eb1632867f811cac
薛斌 <928691436@qq.com>
2021-09-07 01:03:06 +0800

    input双向绑定、修改测试用例

--- test/input.test.js
+++ test/input.test.js
@@ -76,9 +76,12 @@ describe('Input', () => {
                 vm.$on(eventName, callback)
                 //触发change事件
                 let event = new Event(eventName)
+                Object.defineProperty(event,"target",{
+                    value:{value:'hi'},enumerable:true
+                })
                 let inputElement = vm.$el.querySelector('input')
                 inputElement.dispatchEvent(event)
-                expect(callback).to.have.been.calledWith(event)
+                expect(callback).to.have.been.calledWith('hi')
             })
 
         })
