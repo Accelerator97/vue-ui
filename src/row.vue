836ad79942394c315bf9fdd98798ae61f190b81f
薛斌 <928691436@qq.com>
2021-09-07 03:01:37 +0800

    col 和 row的基本框架

---
+++ src/row.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="g-row">
+        <slot />
+    </div>
+</template>
+
+<script>
+export default {
+    name:"gulu-row"
+}
+</script>
+
+<style lang="scss">
+.g-row{
+    display: flex;
+}
+</style>
