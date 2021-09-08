a7e6f80e176b6db23411a050b7627bbb2c0ace84
薛斌 <928691436@qq.com>
2021-09-09 06:14:42 +0800

    toast只有一个

--- src/plugin.js
+++ src/plugin.js
@@ -1,14 +1,21 @@
 import Toast from './toast'
+
+function createToast({Vue,propsData,message}){
+    let Constructor = Vue.extend(Toast)
+    let toast = new Constructor({propsData})
+    toast.$slots.default = [message]
+    toast.$mount()
+    document.body.appendChild(toast.$el)
+    return toast
+}
+
+let currentToast 
+
 export default {
     install(Vue,options){
         Vue.prototype.$toast = function(message,toastOptions){
-            let Constructor = Vue.extend(Toast)
-            let toast = new Constructor({
-                propsData: toastOptions
-            })
-            toast.$slots.default = [message]
-            toast.$mount()
-            document.body.appendChild(toast.$el)
+            if(currentToast){currentToast.close()}
+            currentToast = createToast({Vue,message,propsData:toastOptions})
         }
     }
 }
