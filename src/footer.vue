731a4e6b220ab3627a76daa20df674f2536cf0d6
薛斌 <928691436@qq.com>
2021-09-08 20:06:05 +0800

    布局组件添加

---
+++ src/footer.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="g-footer">
+        <slot />
+    </div>
+</template>
+
+<script>
+export default {
+    name:"gulu-footer"
+}
+</script>
+<style lang="scss">
+    
+</style>
---
+++ src/header.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="g-header">
+        <slot />
+    </div>
+</template>
+
+<script>
+export default {
+    name:"gulu-header"
+
+}
+</script>
+<style lang="scss">
+
+    
+</style>
