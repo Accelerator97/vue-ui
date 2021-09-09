82c1758ea7afa3fd0f794eee55f5ec411667c852
薛斌 <928691436@qq.com>
2021-09-10 03:21:24 +0800

    tabs切换

---
+++ src/tabs.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="g-tabs">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+import Vue from "vue";
+export default {
+  name: "gulu-tabs",
+  props: {
+    selected: {
+      type: String,
+      required: true,
+    },
+    direction: {
+      type: String,
+      default: "horizontal",
+      validator(value) {
+        return ["horizontal", "vertical"].indexOf(value) >= 0;
+      },
+    },
+  },
+  data() {
+    return {
+      eventBus: new Vue(),
+    };
+  },
+  provide() {
+    return {
+      eventBus: this.eventBus,
+    };
+  },
+  mounted() {
+    this.eventBus.$emit("update:selected", this.selected);
+  },
+};
+</script>
+
+<style lang="scss">
+</style>
