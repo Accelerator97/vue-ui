73caac3b857d3aa10016380868567bb657080435
薛斌 <928691436@qq.com>
2021-09-11 22:10:47 +0800

    popover基本功能完成

--- src/popover.vue
+++ src/popover.vue
@@ -1,9 +1,15 @@
 <template>
-  <div class="g-popover" @click="xxx">
-    <div class="g-popover-content-wrapper" v-if="popoverVisible">
+  <div class="g-popover" @click="onClick" ref="popover">
+    <div
+      class="g-popover-content-wrapper"
+      v-if="popoverVisible"
+      ref="contentWrapper"
+    >
       <slot name="content"></slot>
     </div>
-    <slot></slot>
+    <span ref="triggerWrapper">
+      <slot></slot>
+    </span>
   </div>
 </template>
 
@@ -16,8 +22,42 @@ export default {
     };
   },
   methods: {
-    xxx() {
-      this.popoverVisible = !this.popoverVisible;
+    PositionContent() {
+      //popover弹出层位置
+      document.body.appendChild(this.$refs.contentWrapper);
+      let { width, height, top, left } =
+        this.$refs.triggerWrapper.getBoundingClientRect();
+      this.$refs.contentWrapper.style.left = left + window.scrollX + "px";
+      this.$refs.contentWrapper.style.top = top + window.scrollY + "px";
+    },
+    onClickDocument(e) { 
+      if (
+        this.$refs.popover &&
+        (this.$refs.popover === e.target ||
+          this.$refs.popover.contains(e.target))
+      ) {return;}
+      this.close();
+    },
+    open() {
+      this.popoverVisible = true;
+      this.$nextTick(() => {
+        this.PositionContent();
+        document.addEventListener('click',this.onClickDocument)
+      });
+    },
+    close() {
+      this.popoverVisible = false;
+      document.removeEventListener("click", this.onClickDocument);
+    },
+    onClick(event) {
+      if (this.$refs.triggerWrapper.contains(event.target)) {
+        //如果点击的是触发弹出层的按钮
+        if(this.popoverVisible === true) {
+          this.close();
+        }else{
+            this.open()
+        }
+      }
     },
   },
 };
@@ -29,13 +69,12 @@ export default {
   vertical-align: top;
   position: relative;
   border: 1px solid blue;
-  .g-popover-content-wrapper{
-      position: absolute;
-      bottom: 100%;
-      left: 0;
-      border: 1px solid red;
-      box-shadow: 0 0 3px rgba(0,0,0,.5);
-  }
+}
+.g-popover-content-wrapper {
+  position: absolute;
+  border: 1px solid red;
+  box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
+  transform: translateY(-100%);
 }
 </style>
 
